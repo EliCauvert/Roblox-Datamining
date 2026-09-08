b35344ede691829fdd3af2e81b288bc322407e44
Eli Cauvert <eli.cauvert@gmail.com>
2026-09-08 11:46:49 +0200

    JS (2026-09-08 11:46)

--- JS/js.rbxcdn.com/DynamicLocalizationResourceScript_Feature.AccountSettings.js
+++ JS/js.rbxcdn.com/DynamicLocalizationResourceScript_Feature.AccountSettings.js
@@ -1906,6 +1906,8 @@ Roblox.LangDynamic["Feature.AccountSettings"] = {
     "Description.ThirdPartyFriendAccess": "Can developers have access to your friend list?",
     "Action.Copy": "Copy",
     "Message.RecoveryCodesCopied": "Backup Codes Copied to Keyboard",
-    "Action.Download": "Download"
+    "Action.Download": "Download",
+    "Label.PrivatePlaytestSetting": "Private Playtest",
+    "Heading.PrivatePlaytestSetting": "Private playtest setting"
 };
 window.Roblox && window.Roblox.BundleDetector && window.Roblox.BundleDetector.bundleDetected("DynamicLocalizationResourceScript_Feature.AccountSettings");
